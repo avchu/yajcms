bc0db18a281ade01df2c476a9cd1f8e9db03cd1b
Anton Chunikhin <kukowa@gmail.com>
2017-12-26 01:02:28 +0300

    Start entities

---
+++ web/src/main/java/org/yajcms/beans/EntitiesInitializer.java
@@ -0,0 +1,53 @@
+package org.yajcms.beans;
+
+import io.vavr.API;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+@Component
+@Slf4j
+public class EntitiesInitializer {
+
+    List<Resource> resources = List.empty();
+    HashMap<String, JSONObject> entitiesProto = HashMap.empty();
+
+    @PostConstruct
+    public void init() {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        try {
+            resources = List.of(resolver.getResources("/entities/**/*.json"));
+            List.ofAll(resources)
+                    .map(API.unchecked(Resource::getFilename)).forEach(f -> log.debug("{}", f));
+            resources.forEach(x -> {
+                try {
+                    initEntities(x.getFile());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initEntities(File json) {
+        try {
+            entitiesProto.put(json.getName().replace(".json", ""),
+                    new JSONObject(IOUtils.toString(json.toURI(), Charset.forName("utf-8"))));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
